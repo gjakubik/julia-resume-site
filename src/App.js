6945415c15a8c54f9c275e8d8f562c4578a1ce17
Gavin Jakubik <gjakubik@nd.edu>
2021-12-25 01:28:16 -0800

    try this

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="hero-image">
+    <div className='hero-textbox'>
       <h1 className="hero-text">Merry Christmas Julia!</h1>
       <p className="hero-text-2">Whatever site you design, I will make it come to life here :)</p>
+    </div>
     </div>
   );
 }
